5e3e5f30f024a45ecbbaf9eff8e9576aa1a11720
Boutanquoy Xavier <xavier.boutanquoy@mobiapps.fr>
2019-06-20 11:01:09 +0200

    feat(): up gradle and set tabLayout to bottom'

--- app/src/main/java/boutanquoy/xavier/com/gotofit/MainActivity.kt
+++ app/src/main/java/boutanquoy/xavier/com/gotofit/MainActivity.kt
@@ -1,13 +1,9 @@
 package boutanquoy.xavier.com.gotofit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import boutanquoy.xavier.com.gotofit.adapter.FitPagerAdapter
-
-import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
-import android.view.MenuItem
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import boutanquoy.xavier.com.gotofit.adapter.FitPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,27 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         configureTabLayout()
     }
 
     private fun configureTabLayout() {
 
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.fragment_calendar))
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.fragment_chart))
         tab_layout.addTab(tab_layout.newTab().setText(R.string.fragment_measurement))
+        tab_layout.addTab(tab_layout.newTab().setText(R.string.fragment_chart))
+        tab_layout.addTab(tab_layout.newTab().setText(R.string.fragment_calendar))
 
         val adapter = FitPagerAdapter(supportFragmentManager, tab_layout.tabCount)
         pager.adapter = adapter
 
         pager.addOnPageChangeListener(
-            TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+            TabLayout.TabLayoutOnPageChangeListener(tab_layout)
+        )
         tab_layout.addOnTabSelectedListener(object :
             TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
